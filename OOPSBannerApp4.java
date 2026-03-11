b873a3a6ffe8817c1951637711d8ab5decabbd2a
shivangi2205 <shivangi.tripathi2005@gmail.com>
2026-03-11 08:36:30 +0530

    Coded to Render OOPS as Banner using Arrays and Loops

---
+++ OOPSBannerApp4.java
@@ -0,0 +1,17 @@
+/**
+* @Shivangi Developer
+* @version 4.0
+*/
+
+public class OOPSBannerApp4 {
+	public static void main(String[] args) 
+	{
+		lines[0] = System.out.println(String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "));
+		lines[1] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "));
+		lines[2] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "));
+		lines[3] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "));
+		lines[4] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "));
+		lines[5] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "));
+		lines[6] = System.out.println(String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   "));
+	}
+}
